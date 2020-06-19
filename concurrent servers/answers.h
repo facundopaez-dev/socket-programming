269dd855bde4c0baf8688feb934c370fecf923e2
FacuDeveloper <Facu.s.nf@gmail.com>
2020-06-19 02:51:56 -0300

    #6, 1era. verdadera versión de la impl. del prot. de com. y de la int. entre clientes #7 [issues6-7]

--- concurrent servers/answers.h
+++ concurrent servers/answers.h
@@ -1,8 +1,31 @@
-#define ANSWER_TURN_ON "light on\n"
-#define ANSWER_TURN_OFF "light off\n"
-#define ANSWER_I_ENABLE "irrigation enabled\n"
-#define ANSWER_I_DISABLE "irrigation disabled\n"
-#define ANSWER_R_IMAGE "image sended\n"
-#define ANSWER_TAKE_CALL "taken call\n"
-#define ANSWER_EXIT "successful desconnection\n"
-#define ANSWER_PING "I'm listening\n"
+// Respuestas para los emisores
+#define ANSWER_SENDER_TURN_ON "light on\n"
+#define ANSWER_SENDER_TURN_OFF "light off\n"
+#define ANSWER_SENDER_I_ENABLE "irrigation enabled\n"
+#define ANSWER_SENDER_I_DISABLE "irrigation disabled\n"
+#define ANSWER_SENDER_EXIT "successful desconnection\n"
+#define ANSWER_SENDER_PING "I'm listening\n"
+#define ANSWER_SENDER_S_IMAGE "image sent\n"
+#define ANSWER_SENDER_CALL_TO "call sent\n"
+#define ANSWER_SENDER_SEND_AUDIO "audio sent\n"
+#define ANSWER_SENDER_ID "your ID is "
+
+// Respuestas para los receptores
+#define ANSWER_RECEIVER_R_IMAGE "image received\n"
+#define ANSWER_RECEIVER_TAKE_CALL "call taken\n"
+#define ANSWER_RECEIVER_REC_AUDIO "audio received\n"
+
+// Avisos para los receptores
+#define IMAGE_NOTICE_TO_RECEIVER_FIRST_PART "the client of the department "
+#define IMAGE_NOTICE_TO_RECEIVER_SECOND_PART " sent you an image\n"
+#define CALL_NOTICE_TO_RECEIVER_FIRST_PART "the client of the department "
+#define CALL_NOTICE_TO_RECEIVER_SECOND_PART " is calling you\n"
+#define AUDIO_NOTICE_TO_RECEIVER_FIRST_PART "the client of the department "
+#define AUDIO_NOTICE_TO_RECEIVER_SECOND_PART " sent you an audio\n"
+
+// Respuestas generales
+#define ANSWER_INVALID_COMMAND "Invalid command\n"
+#define ANSWER_LIMIT_CONNECTIONS "Connection limit reached, try again later\n"
+#define ANSWER_NO_DEPARTMENT "department number does not exist\n"
+#define ANSWER_CLIENT_NOT_CONNECTED "the requested client is not connected\n"
+#define ANSWER_EQUAL_DEPARTMENT "Can't send a message to the same department\n"
